00635577e9cefed3d6ed39559351b7269603a5f9
Rohit Singh Rathaur <rohitrathore.imh55@gmail.com>
2020-03-28 00:21:43 +0530

    implement search contact fiunctionality

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,23 @@
+export const initialState = {
+    people: [],
+    profileActive: true,
+    editActive: false,
+    searchKey: ""
+  };
+  export default (state, action) => {
+    const { payload } = action;
+    switch (action.type) {
+      case "people":
+        return { ...state, people: payload };
+      case "profileTab":
+        return { ...state, ...payload };
+      case "editTab":
+        return { ...state, ...payload };
+      case "person":
+        return { ...state, person: payload };
+      case "searchKey":
+        return { ...state, searchKey: payload };
+      default:
+        return state;
+    }
+  };
